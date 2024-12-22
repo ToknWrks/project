81c9879cb46960c3b654c970789aa959123f5f14
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2024-12-22 04:46:34 -0500

    Update lib/messages/claim-rewards.ts [BoltSync]

--- lib/messages/claim-rewards.ts
+++ lib/messages/claim-rewards.ts
@@ -1,5 +1,4 @@
 import { MsgWithdrawDelegatorReward } from "cosmjs-types/cosmos/distribution/v1beta1/tx";
-import { coins } from "@cosmjs/proto-signing";
 
 export interface ClaimRewardsParams {
   delegatorAddress: string;
