06dfe212235b5a68f43ffcf4e9098b996fb5bb3d
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2024-12-23 12:11:20 -0500

    Update lib/constants/coingecko.ts [BoltSync]

---
+++ lib/constants/coingecko.ts
@@ -0,0 +1,22 @@
+export const CHAIN_TO_COINGECKO_ID: { [key: string]: string } = {
+  'osmosis': 'osmosis',
+  'cosmoshub': 'cosmos',
+  'celestia': 'celestia',
+  'akash': 'akash-network',
+  'regen': 'regen',
+  'juno': 'juno-network',
+  'dydx': 'dydx',
+  'saga': 'saga-2',
+  'omniflixhub': 'omniflix-network',
+  'stargaze': 'stargaze',
+  'stride': 'stride',
+  'sentinel': 'sentinel',
+  'archway': 'archway',
+  'noble': 'noble',
+  'persistence': 'persistence',
+  'evmos': 'evmos',
+  'injective': 'injective-protocol',
+  'secret': 'secret',
+  'terra': 'terra-luna-2',
+  'kujira': 'kujira'
+} as const;
